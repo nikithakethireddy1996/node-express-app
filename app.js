ca0d3dff66edd7020ddb44cc4f65677d3bc0dea8
nikithakethireddy1996 <54380887+nikithakethireddy1996@users.noreply.github.com>
2019-10-21 18:52:09 -0500

    Adding necessary changes

--- app.js
+++ app.js
@@ -18,54 +18,54 @@ const port = process.env.PORT || config.get("port");
 
 // declare your callback function the old way
 app.get('/', function (req, res) {
-  res.send('Welcome to the default page!  <br> <br>' +
+  res.send('<h3 style="color:green">Welcome to the default page!  <br> <br>' +
     'Try going to different URIs by adding these at the end: <br> <br>' +
-    '/hello <br>' +
-    '/big <br>' +
-    '/json <br>' +
-    '/greeting/yourname <br>' +
-    '/yo/Dr.Rogers <br>' +
+    '/hey ya <br>' +
+    '/mega <br>' +
+    '/respond <br>' +
+    '/wish/yourname <br>' +
+    '/hey-you/Dr.Rogers <br>' +
     '/fortune <br>' +
     '/fancy/?first=Denise&last=Case <br>' +
-    '<br> <br>' +
+    '<br> <br> </h3>' +
     'Fork the source code from <a href="https://github.com/denisecase/node-express-app">https://github.com/denisecase/node-express-app</a>'
   )
 })
 
 // or use the new arrow function syntax
 // respond with text
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+app.get('/hey ya', (req, res) => {
+  res.send('<h4>Hii World!</h4>')
 })
 
 // or respond with html
-app.get('/big', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/mega', (req, res) => {
+  res.send('<h1 style="color:green">Hey World!</h1>')
 })
 
 // or respond with JSON
-app.get('/json', (req, res) => {
-  res.send('{"name" : "Nandini"}')
+app.get('/respond', (req, res) => {
+  res.send('<h4>{"Name" : "Nikitha"}</h4>')
 })
 
 // :name indicates a parameter at this location in the URI
-app.get('/greeting/:id', (req, res) => {
-  res.send(`Hello! The id provided was ${req.params.id}.`)
+app.get('/wish/:id', (req, res) => {
+  res.send(`<h4>Hi! Your name will always be ${req.params.id}.</h4>`)
 })
 
 // combine your skills and get creative
-app.get('/yo/:buddy', (req, res) => {
-  res.send(`<h1>Yo, ${req.params.buddy}!</h1>`)
+app.get('/hey-you/:buddy', (req, res) => {
+  res.send(`<h1 style="color:green">Bojour!, ${req.params.buddy}!</h1>`)
 })
 
 // provide multiple query parameters (named first and last) with ? and &
 app.get('/fancy', (req, res) => {
   const first = req.query.first
   const last = req.query.last
-  res.send(`Hello ${first} ${last}!`)
+  res.send(`<h4>Hey ${first} ${last}!</h4>`)
 })
 
-let fortunes = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.',
+let fort = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.',
 'You may rely on it', 'As I see it, yes.', 'Most likely', 'Outlook good.', 'Yes.', 'Signs point to yes.',
 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 
 'Concentrate and ask again.', 'Don\'t count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.',
@@ -74,11 +74,11 @@ let fortunes = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Ye
 // Implements a Magic 8 Ball service
 app.get('/fortune', (req,res) => {
   if(isEmpty(req.query)){
-    res.send('<h2>You wish to know the future?</h2>' +
-             '<p>Ask a question in the query string, e.g., http://localhost:3002/fortune?Will I become rich? <br/>' +
-             '<p>The Magic 8 Ball will answer!</p>')
+    res.send('<h2 style="color:green">You wish to know the future?</h2>' +
+             '<p style="color:green">Ask a question in the query string, e.g., http://localhost:3002/fortune?Will I become rich? <br/>' +
+             '<p style="color:green">The Magic 8 Ball will answer!</p>')
   } else {
-    res.send(`The answer is ... wait for it ... ${fortunes[randomInt(0, fortunes.length)]}`)
+    res.send(`<h1>The answer is ... wait for it ... ${fort[randomInt(0, fort.length)]}</h1>`)
   }
 })
 
@@ -92,12 +92,12 @@ app.use((req, res, next) => {
 app.listen(port, hostname, () => {
   console.log(`\n App listening at http://${hostname}:${port}/`)
   console.log(`\n Try going to different URIs:\n`)
-  console.log(`   Try /hello`)
-  console.log(`   Try /big`)
-  console.log(`   Try /json`)
+  console.log(`   Try /hey ya`)
+  console.log(`   Try /mega`)
+  console.log(`   Try /resond`)
   console.log(`   Try /fortune`)
-  console.log(`   Try /greeting/yourname`)
-  console.log(`   Try /yo/Dr.Rogers`)
+  console.log(`   Try /wish/yourname`)
+  console.log(`   Try /hey-you/Dr.Rogers`)
   console.log(`   Try /fancy/?first=Denise&last=Case`)
   console.log('\n Hit CTRL-C CTRL-C to stop\n')
 })
